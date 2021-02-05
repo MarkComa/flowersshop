b5ccc01498515f6cdfa564f5005a205cd113bbb3
Alexandr <65395510+MarkComa@users.noreply.github.com>
2021-02-05 12:27:21 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
+$(document).ready(function() {
+
  const flowersSlider = new Swiper('.flowers__slider', {
   // Optional parameters
   loop: true,
   slidesPerView: 6,
+
+  //активация управления с клавиатуры
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  //Настройки адаптации
+
+    breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 4,   
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 6,   
+    }
+  },
 
   // Navigation arrows
   navigation: {
@@ -19,4 +48,29 @@
     nextEl: '.swiper-btn-next',
     prevEl: '.swiper-btn-prev',
   },
+});
+
+$("#review-1").on('click', function() {
+
+  $.fancybox.open([
+    {
+      src  : 'https://chelyabinsk.cvety-love.ru/wp-content/uploads/2020/03/buket-nevesty-iz-pionov-i-pionovidnyh-roz.jpg',
+      opts : {
+        caption : 'First caption',   
+      }
+    },
+    {
+      src  : 'https://birgelinlik.com/assets/upload/2018/03/gelinlik-modeli-74a892689445790af778f6f7f0f2c17f.jpg',
+      opts : {
+        caption : 'Second caption',
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+});
 });
